c2b7415c0e98efef0fade6a973d8b56959575b8e
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-02 19:50:51 +0300

    Even more clean

--- create/static/create/script.js
+++ create/static/create/script.js
@@ -1,80 +1,75 @@
 "use strict"
 class MovableDiv {
 
-	constructor(width, height, background,  marginLeft="inherit") {
+	constructor(width, height, background) {
 		this.element = document.createElement('div');
 		this.element.classList.add("movable-div");
 
+		this.element.style.display = "inline-block";
 		this.element.style.width = width + "px";
 		this.element.style.height = height+ "px";
 		this.element.style.background = background;
 		this.element.style.margin = "0 20px 0 20px";
-		this.element.style.marginLeft = marginLeft;
+		this.element.style.zIndex = 1000;
 
-		this.element.addEventListener('click', this.methods.click);
 		this.element.addEventListener('mousedown', this.methods.mousedown);
 	}
 
-	methods = {
-		click: (ev) => {
+	helpers = {
+		// log: () => {
+		// 	console.log(
+		// 		{
+		// 			message: "Clicked!",
+		// 			distanceTop: this.element.offsetTop + "px",
+		// 			distanceLeft: this.element.offsetLeft + "px",
+		// 			outline: this.element.style.outline
+		// 		}
+		// 	)
+		// } ,
+		removeOutlineOnOthers: () => {
+			let elements = app.querySelectorAll("*");
 
-			const submethods = {
-				log: () => {
-					console.log(
-						{
-							message: "Clicked!",
-							distanceTop: this.element.offsetTop + "px",
-							distanceLeft: this.element.offsetLeft + "px",
-							outline: this.element.style.outline
-						}
-					)
-				} ,
-				removeOutlineOnOthers: () => {
-					let elements = app.querySelectorAll("*");
-	
-					for (let i=0, len = elements.length; i< len; i++){
-						if(elements[i].classList.contains("outline")){
-							elements[i].classList.remove("outline")
-						}
-					}
-				},
-				outline: () => {
-			
-					if(this.element.classList.contains("outline")){
-						this.element.classList.remove("outline")
-					} else {
-						this.element.classList.add("outline")
-					}
-					
-					
-				},
+			for (let i=0, len = elements.length; i< len; i++){
+				if(elements[i].classList.contains("outline")){
+					elements[i].classList.remove("outline")
+				}
 			}
-	
-			submethods.log()
-			submethods.removeOutlineOnOthers()
-			submethods.outline()
-		
 		},
+		outline: () => {
+	
+			if(this.element.classList.contains("outline")){
+				this.element.classList.remove("outline")
+			} else {
+				this.element.classList.add("outline")
+			}
+			
+			
+		},
+	}
+
+	methods = {
 		mousemove : (ev) => {
 			console.log("Mousemoving!")
 			
-			const submethods = {
+			const subhelpers = {
 				moveAt : (pageX, pageY) => {
 					this.element.style.left = pageX - this.offsetWidth / 2 + 'px';
 					this.element.style.top = pageY - this.offsetHeight / 2 + 'px';
 				}
 			}
-			console.log(ev.pageX, ev.pageY);
 
-			submethods.moveAt(ev.pageX, ev.pageY)
+			subhelpers.moveAt(ev.pageX, ev.pageY)
 
 		},
 		mousedown : (ev) =>{
-			this.element.style.position = 'absolute';
+	
+			// helpers.log()
+			this.helpers.removeOutlineOnOthers()
+			this.helpers.outline()
 
 			this.element.addEventListener('mouseup', this.methods.mouseup);
 			this.element.addEventListener('mousemove', this.methods.mousemove);
-
+			this.element.addEventListener('mouseleave', this.methods.mouseleave);
 		
 			this.element.style.marginLeft = ev.pageX - this.offsetWidth / 2 + 'px';
 			this.element.style.marginTop = ev.pageY - this.offsetHeight / 2 + 'px';
@@ -84,18 +79,22 @@ class MovableDiv {
 			
 		},
 		mouseup: (ev) => {
-			console.log(this)
+			this.helpers.removeOutlineOnOthers()
+			console.log("Mouseup")
 			this.element.removeEventListener("mousemove", this.methods.mousemove); // works now!
-			this.element.style.background="red"
+		},
+		mouseleave: (ev) => {
+			this.helpers.removeOutlineOnOthers()
+			this.element.removeEventListener("mousemove", this.methods.mousemove); // works now!
 		}
+
 	}
 
-
-	
 }
 
 var app = document.querySelector("#app");
 var content =  document.querySelector("#content");
-content.appendChild(new MovableDiv(200, 100, "#A5A5A5", "150px").element)
+content.appendChild(new MovableDiv(200, 100, "#676867").element)
+content.appendChild(new MovableDiv(200, 100, "#A5A5A5").element)
 
 
